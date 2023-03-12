9602dd2400a1332239abd34780fcd9d24c352eb4
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-03-13 04:21:43 +0530

    added event listener to search bar

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// Public Key
+// 8ef3c799dfb791ffe453fdf363f2b764
+
+// Private Key
+// ac76bbd5a45201d2168ef0988b374f2a7532d740
+
+// importing cryptoJS for creation of hash
+const MD5 = require('crypto-js/md5')
+
+// getting the elements from document
+let searchBar = document.getElementById('search-bar');
+let searchResults = document.getElementById('search-results');
+
+// adding event listener to search bar
+searchBar.addEventListener('input', () => searchHeroes(searchBar.value));
+
+// searchHeroes function
+async function searchHeroes(textSearched){
+    let Public_Key = '8ef3c799dfb791ffe453fdf363f2b764';
+    let Private_Key = 'ac76bbd5a45201d2168ef0988b374f2a7532d740';
+    let ts = new Date().getTime();
+    let hash = MD5(ts + Private_Key + Public_Key).toString();
+}
