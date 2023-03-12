cecf17e1bcde303f441f8a379434956592ab27b7
arjunbraj <arjunbaburaj.9701@gmail.com>
2023-03-13 05:24:05 +0530

    added function for removing hero from favourites

--- js/favourites.js
+++ js/favourites.js
@@ -66,3 +66,45 @@ function addEvent() {
     let characterInfo = document.querySelectorAll(".character-info");
     characterInfo.forEach((character) => character.addEventListener("click", addInfoInLocalStorage));
 }
+
+function removeCharacterFromFavourites() {
+     
+    // Storing the Id of character in a voriable
+    let idOfCharacterToBeDeleted = this.parentElement.children[2].innerHTML.substring(5);
+
+    // getting the favourites array which stores objects of character  
+    let favourites = JSON.parse(localStorage.getItem("favouriteCharacters"));
+    // favouritesCharacterIDs is taken from localStorage for deleting the ID of the character which is deleted from favourites
+    let favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+    // deleting the characters id from favouritesCharacterId map
+    favouritesCharacterIDs.delete(`${idOfCharacterToBeDeleted}`);
+
+
+    // deleting the character form array whose id is matched 
+    favourites.forEach(function (favourite, index) {
+         if (favourite.id == idOfCharacterToBeDeleted) {
+              // console.log(favourite)
+              favourites.splice(index, 1);
+         }
+    });
+
+    // if all the characters are deleted from favourites and not character left for displaying
+    if (favourites.length == 0) {
+         cardContainer.innerHTML = "<p class=\"no-characters\">No characters present in Favourites</p>";
+    }
+    // console.log(favourites);
+
+    // Updating the new arrays in localStorage
+    localStorage.setItem("favouriteCharacters", JSON.stringify(favourites));
+    localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+
+    // Removing the element from DOM
+    this.parentElement.remove();
+
+    // displaying the "Removed from favourites" toast in DOM
+    document.querySelector(".remove-toast").setAttribute("data-visiblity", "show");
+    // Removing the "Removed from favourites" toast form DOM
+    setTimeout(function () {
+         document.querySelector(".remove-toast").setAttribute("data-visiblity", "hide");
+    }, 1000);
+}
